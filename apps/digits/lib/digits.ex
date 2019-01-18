5da1e5b4663c169f9b21f9c7696f293e60e70a89
Predrag Rakic <predrag.rakic@gmail.com>
2019-01-18 19:15:25 +0100

    DG: initial commit

---
+++ apps/digits/lib/digits.ex
@@ -0,0 +1,18 @@
+defmodule Digits do
+  @moduledoc """
+  Documentation for Digits.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Digits.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
